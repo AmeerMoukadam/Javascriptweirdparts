aea1ac4c70a6037dca4a4e0ca6900982da93b31a
Ameer Mouky <ameer.mouky@yahoo.com>
2020-04-08 20:59:33 -0700

    Hoisting in app.js

--- B4_Hoisting/Starter/app.js
+++ B4_Hoisting/Starter/app.js
@@ -1,5 +1,23 @@
+b();
+console.log(a);
+
+
 var a = 'Hello World!';
 
 function b() {
-    
+    console.log('Called b!');
 }
+// the code above will run fine, however due to hoisting,
+// the value of the variable will be undefined but the function
+// will run fine so it is better to write like this below
+
+
+
+var a = 'Hello World!';
+
+function b() {
+    console.log('Called b!');
+}
+
+b();
+console.log(a);
